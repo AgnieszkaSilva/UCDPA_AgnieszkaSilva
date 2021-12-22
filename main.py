08fab6e8bdb7e65ad14d3d94c3af8dc332d9e5f6
Agnieszka Silva <agnieszka.silva1@gmail.com>
2021-12-22 23:30:46 +0100

    Data visualisation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from datetime import datetime
 
 # Reading Disney Movies CSV
@@ -27,6 +29,20 @@ print(dm_income_adj.head())
 # Adding a new column that shows the difference between the total and inflation adjusted gross incomes
 new_dm["adjusted_gross_difference"] = new_dm["inflation_adjusted_gross"] - new_dm["total_gross"]
 print(dm)
+
+# Setting default chart styles via Seaborn
+sns.set_style("dark")
+sns.set_palette("pastel")
+
+# Creating a line chart to visualise the difference between the total and inflation adjusted gross incomes
+dm_indexed = new_dm.set_index("release_date", drop=False)
+dm_indexed.index = pd.to_datetime(dm_indexed.index)
+fig, ax = plt.subplots()
+ax.plot(dm_indexed.index, dm_indexed["total_gross"], label="Total gross (USD)")
+ax.plot(dm_indexed.index, dm_indexed["inflation_adjusted_gross"], label="Inflation adjusted gross (USD)")
+ax.set_title("Total and inflation adjusted gross income over time")
+plt.legend()
+plt.show()
 
 # Setting an index and sorting based on that index, before slicing
 dm_srt = new_dm.set_index("release_date", drop=False).sort_index()
@@ -61,6 +77,18 @@ dm_decade_count = np.array([
 ])
 print(dm_decade_count)
 
+# Creating a bar chart to compare the number of movies for each decade
+dm_decades = ["30s", "40s", "50s", "60s", "70s", "80s", "90s", "00s", "10s"]
+fig, ax = plt.subplots()
+hbars = ax.barh(dm_decades, dm_decade_count)
+ax.invert_yaxis()
+ax.set_xlabel("Number of movies")
+ax.set_ylabel("Decade")
+ax.bar_label(hbars)
+ax.set_xlim(right=275) # Adjust limit to fit labels
+ax.set_title("Movies per decade")
+plt.show()
+
 # Calculating the total gross income for each decade
 dm_decade_sum = np.array([
     dm_srt["1937":"1940"]["total_gross"].sum(),
@@ -75,6 +103,20 @@ dm_decade_sum = np.array([
 ])
 print(dm_decade_sum)
 
+# Creating a line chart to visualise the number of movies as well as total gross income for each decade
+with sns.axes_style("darkgrid"):
+    fig, ax1 = plt.subplots()
+    ax1.set_title("Movies and income per decade")
+    ax1.set_xlabel("Decade")
+    ax1.plot(dm_decades, dm_decade_count, label="Number of movies", marker="o", linestyle="--", color="orange")
+    ax1.set_ylabel("Number of movies")
+    ax1.legend(loc="upper left")
+    ax2 = ax1.twinx()
+    ax2.plot(dm_decades, dm_decade_sum, label="Total gross income (USD)", marker="s", linewidth=0.5, color="blue")
+    ax2.set_ylabel("Total gross income (USD)")
+    ax2.legend(loc="lower right")
+    plt.show()
+
 # Grouping by genre to compare the total gross income (sum)
 dm_grouped_sum = new_dm.groupby("genre")["total_gross"].sum()
 print(dm_grouped_sum)
@@ -86,6 +128,20 @@ print(dm_grouped_mean)
 # Creating a dictionary from an existing Series
 dm_grouped_dict = dm_grouped_sum.to_dict()
 print(dm_grouped_dict)
+
+# Creating a pie chart to visualise the 5 genres that generated the highest gross income
+dm_grouped_sum_top5 = dm_grouped_sum.nlargest(5)
+fig, ax = plt.subplots()
+ax.pie(dm_grouped_sum_top5, labels=dm_grouped_sum_top5.index)
+ax.set_title("Top 5 genres (total gross income)")
+plt.show()
+
+# Creating a pie chart to visualise the 5 genres that generated the highest gross income in average
+dm_grouped_mean_top5 = dm_grouped_mean.nlargest(5)
+fig, ax = plt.subplots()
+ax.pie(dm_grouped_mean_top5, labels=dm_grouped_mean_top5.index)
+ax.set_title("Top 5 genres (average gross income)")
+plt.show()
 
 # Reusable function to calculate how many years ago a date is
 def years_ago(date):
